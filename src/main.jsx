c05bc904c2ed08cf87d9e198416054dd2622464b
heeyeon <heeyeon8662@gmail.com>
2024-08-16 19:10:41 +0900

    yujin

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-//yujin test 222
+//yujin test
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
