f6dd5ab360d05ceb2d9cf766b0de0c5455aef3e4
yujin <wowyj26@naver.com>
2024-08-17 23:28:51 +0900

    feat: 좌석표 선택 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import Seat from "./Components/reservation/Seat";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/reservation" element={<Seat />} />
         </Routes>
       </Router>
     </>
