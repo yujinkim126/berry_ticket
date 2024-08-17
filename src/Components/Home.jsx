4d8e9c1a126638d84008e145b0347211b9d2a74b
heeyeon <heeyeon8662@gmail.com>
2024-08-18 00:41:11 +0900

    메인브랜치와 충돌으로 개인브랜치에 수정사항 커밋

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,146 @@
-import { getConcerts } from "../common";
-
 const Home = () => {
-  const testAxios = () => {
-    getConcerts().then((res) => {
-      console.log(res);
-    });
-  };
-
+  // const testAxios = () => {
+  //   fetch("/api/concerts")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // };
   return (
     <div>
-      <button onClick={testAxios}>테스트</button>
+      {/* <!-- Header--> */}
+      <header className="bg-dark py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="text-center text-white">
+            <h1 className="display-4 fw-bolder">Shop in style</h1>
+            <p className="lead fw-normal text-white-50 mb-0">
+              With this shop hompeage template
+            </p>
+          </div>
+        </div>
+      </header>
+      {/* <!-- Section--> */}
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* <!-- Product image--> */}
+                <img
+                  className="card-img-top"
+                  src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                  alt="..."
+                />
+                {/* <!-- Product details--> */}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* <!-- Product name--> */}
+                    <h5 className="fw-bolder">Fancy Product</h5>
+                    {/* <!-- Product price--> */}
+                    $120.00 - $280.00
+                  </div>
+                </div>
+                {/* <!-- Product actions--> */}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#">
+                      View options
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* <!-- Sale badge--> */}
+                <div
+                  className="badge bg-dark text-white position-absolute"
+                  style={{ top: "0.5rem", right: "0.5rem" }}
+                >
+                  Sale
+                </div>
+                {/* <!-- Product image--> */}
+                <img
+                  className="card-img-top"
+                  src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                  alt="..."
+                />
+                {/* <!-- Product details--> */}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* <!-- Product name--> */}
+                    <h5 className="fw-bolder">Special Item</h5>
+                    {/* <!-- Product reviews--> */}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                    </div>
+                    {/* <!-- Product price--> */}
+                    <span className="text-muted text-decoration-line-through">
+                      $20.00
+                    </span>
+                    $18.00
+                  </div>
+                </div>
+                {/* <!-- Product actions--> */}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* <!-- Product image--> */}
+                <img
+                  className="card-img-top"
+                  src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                  alt="..."
+                />
+                {/* <!-- Product details--> */}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* <!-- Product name--> */}
+                    <h5 className="fw-bolder">Popular Item</h5>
+                    {/* <!-- Product reviews--> */}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                      <div className="bi-star-fill"></div>
+                    </div>
+                    {/* <!-- Product price--> */}
+                    $40.00
+                  </div>
+                </div>
+                {/* <!-- Product actions--> */}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- Footer--> */}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright &copy; Your Website 2023
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
