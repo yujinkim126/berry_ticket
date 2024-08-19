7e8ed52a6aac1d6e9a2956f87f90e4b3e29aeaa3
yujin <wowyj26@naver.com>
2024-08-19 15:13:44 +0900

    feat: 전체 mockData 생성 및 함수 선언, 미사용 server파일 삭제

--- server/mockData.js
+++
@@ -1,6 +0,0 @@
-const concertData = {
-  concertId: 12,
-  title: "박희연의 콘서트!!",
-};
-
-export default concertData;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,6 +2,58 @@
 import { http, HttpResponse } from "msw";
 import mockData from "./mockData.json";
 
+// 유저 대기열 토큰 조회 API
+// param {userId}
+const getTokenHandler = http.get("/api/token", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.getToken,
+  });
+});
+
+// 유저 대기열 토큰 발급 API
+// requestBody {userId}
+const postTokenHandler = http.post("/api/token", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.postToken,
+  });
+});
+
+// 유저 잔액 조회 API
+// param {userId}
+const getBalanceHandler = http.get("/api/balance", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.getBalance,
+  });
+});
+
+// 잔액 충전 요청 API
+const putBalanceChargeHandler = http.put("/api/balance/charge", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.postBalanceCharge,
+  });
+});
+
+// 콘서트 조회 API 핸들러
 const getConcertsHandler = http.get("/api/concerts", () => {
   return HttpResponse.json({
     code: "OK",
@@ -13,5 +65,73 @@ const getConcertsHandler = http.get("/api/concerts", () => {
   });
 });
 
+// 해당 날짜 예약 가능 좌석 조회 API
+// param {userId, scheduleId}
+const getConcertsSeatsHandler = http.get("/api/concerts/seats", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.getConcertsSeats,
+  });
+});
+
+// 예약 가능 날짜 조회 API
+// param {userId, contentId}
+const getConcertsSchedulesHandler = http.get("/api/concerts/chedules", () => {
+  return HttpResponse.json({
+    code: "OK",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    concerts: mockData.getConcertsSchedules,
+  });
+});
+
+// 좌석 예약 요청 API
+// param {userId, reqVo={userId, contentId, scheduleId, seatIdList}}
+const postReservationsSeatsHandler = http.post(
+  "/api/reservations/seats",
+  () => {
+    return HttpResponse.json({
+      code: "OK",
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      concerts: mockData.postReservationSeats,
+    });
+  }
+);
+
+// 결제 API
+// param: {userId, reqVo={userId, reservationId, amount}}
+const postReservationsPaymentsHandler = http.post(
+  "/api/reservations/payments",
+  () => {
+    return HttpResponse.json({
+      code: "OK",
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      concerts: mockData.postReservationsPayments,
+    });
+  }
+);
+
 // 핸들러를 배열로 내보냅니다
-export const handlers = [getConcertsHandler];
+export const handlers = [
+  getConcertsHandler,
+  getConcertsSeatsHandler,
+  getConcertsSchedulesHandler,
+  postReservationsSeatsHandler,
+  postReservationsPaymentsHandler,
+  getTokenHandler,
+  postTokenHandler,
+  getBalanceHandler,
+  putBalanceChargeHandler,
+];
