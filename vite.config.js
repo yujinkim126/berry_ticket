b480719ec5153aeab3043784f0eea49c400beca0
heeyeon <heeyeon8662@gmail.com>
2024-08-23 13:42:52 +0900

    build: shadcn/ui 설치 및 환경구성

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// 현재 모듈의 디렉토리 경로를 가져오기 위한 __dirname 유사 구현
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve("./src/components"),
+      "@ui": path.resolve("./src/components/ui"),
+      "@public": path.resolve("./public"), // public 폴더 경로
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
