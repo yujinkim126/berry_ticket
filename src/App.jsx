b618c7610cbbf0064af6aaead7a469c46345ae0c
yujin <wowyj26@naver.com>
2024-08-27 17:02:59 +0900

    feat: 모달/팝업 컴포넌트 생성 및 스토어 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "@components/Header";
+import GlobalModal from "./Components/common/GlobalModal";
+import GlobalPopup from "./Components/common/GlobalPopup";
 
 function App() {
   return (
     <div>
       <Header />
       <Outlet />
+      <GlobalModal />
+      <GlobalPopup />
     </div>
   );
 }
