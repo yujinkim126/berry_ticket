249f105f97e24ac31b84d5b6f371cf154714117a
yujin <wowyj26@naver.com>
2024-08-28 16:00:06 +0900

    feat: 콘서트 리스트 조회화면 스켈레톤 적용

--- src/Components/concertList/ConcertListSkeleton.jsx
+++ src/Components/concertList/ConcertListSkeleton.jsx
@@ -1,39 +1,37 @@
-import Skeleton from "react-loading-skeleton";
-
 const ConcertListSkeleton = () => {
+  // count에 따라 스켈레톤 카드 개수 설정
+  const cardCount = 30;
+
   return (
     <div className="font-[sans-serif] my-4">
       <div className="max-w-5xl max-lg:max-w-2xl mx-auto mt-10">
-        <div className="max-w-2xl mx-auto text-center">
-          <Skeleton />
-        </div>
+        <div className="max-w-2xl mx-auto text-center"></div>
         <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-8 max-md:justify-center mt-12">
-          {/** 콘서트 카드 */}
-          <div className="border rounded-md overflow-hidden max-md:max-w-[300px] transform transition duration-500 hover:scale-110">
-            <Skeleton className=" w-full h-50 object-contain object-top bg-gray-200" />
-            <div className="p-4">
-              <h4 className="text-gray-800 text-base font-bold min-h-[48px] max-h-[100px]">
-                {/* {concert.title} */}
-              </h4>
-              <div className="mt-4">
-                <p className="text-gray-900 text-base leading-relaxed">
-                  {/* {concert.placeNm} */}
-                </p>
-              </div>
-
-              <div className="mt-4">
-                <button
-                  type="button"
-                  className="px-5 py-2.5 rounded-full text-white text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-blue-800 active:bg-blue-700"
-                >
-                  예매하기
-                </button>
+          {/* 카드 전체 영역 */}
+          {Array.from({ length: cardCount }).map((_, index) => (
+            <div
+              key={index}
+              className="border rounded-md overflow-hidden max-md:max-w-[300px] transform transition duration-500"
+            >
+              {/* 이미지 */}
+              <div className="w-full min-h-[450px] object-contain object-top bg-gray-200" />
+              {/* 타이틀 */}
+              <div className="p-4">
+                <h4 className="bg-gray-200 max-w-[200px]  min-h-[48px] max-h-[80px]"></h4>
+                {/* 버튼 */}
+                <div className="mt-8">
+                  <button
+                    type="button"
+                    className="min-w-24 min-h-11 px-5 py-2.5 rounded-full tracking-wider outline-none bg-gray-200"
+                  ></button>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
+
 export default ConcertListSkeleton;
